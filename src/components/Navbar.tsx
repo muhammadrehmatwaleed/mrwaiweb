e0c5a5cd8d9ec75437e6228616a2466ad70df49a
muhammadrehmatwaleed <muhammadrehmatwaleed.367@gmail.com>
2025-08-25 14:14:49 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,9 +40,11 @@ export default function Navbar() {
     }`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <div className="font-bold text-2xl bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-            MRW
-          </div>
+        <img
+        src="/mrwaiimage.jpg"
+        alt="Logo"
+        className="h-14 w-16"
+        />
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex space-x-8">
